5000d3637e5a1f794fbbfd896e15500ba7519ad7
fangweiyu <fangweiyu@paisongtech.com>
2021-06-25 15:14:11 +0800

    aws go sdk test
    get user info by AdminGetUser or ListUsers

---
+++ go.mod
@@ -0,0 +1,5 @@
+module main.go
+
+go 1.15
+
+require github.com/aws/aws-sdk-go v1.38.67
--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
+)
+
+var (
+	username = "johndoe"
+	id = "id"
+	secret = "secret"
+	sub = "sub = \"sub\""
+	userpoolid = "userpollid"
+	attr = "email"
+	attributesToGet = []*string{&attr}
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	// https://docs.aws.amazon.com/sdk-for-go/v1/developer-guide/configuring-sdk.html#specifying-credentials
+	creds := credentials.NewStaticCredentials(id,secret,"")
+	// https://docs.aws.amazon.com/sdk-for-go/v1/developer-guide/sessions.html
+	// https://docs.aws.amazon.com/sdk-for-go/v1/developer-guide/configuring-sdk.html#creating-sesstion
+	mySession := session.Must(session.NewSession(&aws.Config{Region: aws.String("us-east-2"),Credentials:creds}))
+
+	cip := cognitoidentityprovider.New(mySession)
+	//agui := cognitoidentityprovider.AdminGetUserInput{UserPoolId:&userpoolid,Username:&username}
+	lui := cognitoidentityprovider.ListUsersInput{UserPoolId:&userpoolid, Filter: &sub, AttributesToGet:attributesToGet}
+
+	//user, err := cip.AdminGetUser(&agui)
+	user, err:= cip.ListUsers(&lui)
+	if err != nil {
+		fmt.Print(err)
+	}
+	fmt.Print(user)
 }
